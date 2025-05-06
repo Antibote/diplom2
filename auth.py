6d609259b5b94d1fb64fff68d0c2eba85b06696b
yasel <yaselivoxin@gmail.com>
2025-05-06 11:14:07 +0300

    Неактивные пользователи больше не могут аутентифицироваться

--- auth.py
+++ auth.py
@@ -43,6 +43,9 @@ async def get_current_user(
     if user is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
 
+    if not user.is_active:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User is not active")
+
     return user
 
 
